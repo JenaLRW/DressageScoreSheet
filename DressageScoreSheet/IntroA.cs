8414f4a5aa93c5ffa109ea10e076cbf8f7239283
[Jena White] <[jenale@gmail.com]>
2024-06-06 00:16:46 -0400

    Entered all tests' movements and their co-efficients. No math yet.

---
+++ DressageScoreSheet/IntroA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DressageScoreSheet
+{
+    internal class IntroA //USEF Introductory Test A
+    {
+        public int IAMove1 { get; set; } 
+        public int IAMove2 { get; set; }
+        public int IAMove3 { get; set; }
+        public int IAMove4 { get; set; }
+        public int IAMove5 { get; set; }
+        public int IAMove6 { get; set; }
+        public int IAMove7 { get; set; }
+        public int IAMove8 { get; set; }   
+        public int IAMove9 { get; set; }
+
+
+   
+    
+    }
+}
