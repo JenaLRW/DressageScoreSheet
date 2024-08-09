72aa1f1b07bec3d3c742d05e2fff7180ab1d36f2
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-08-09 10:10:49 -0400

    API app. Far from done
    but coming along.... I think?

---
+++ ConnectingDressageScoringAPI.cs
@@ -0,0 +1,20 @@
+﻿namespace scoringapi
+{
+    public class ConnectingDressageScoringAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public ConnectingDressageScoringAPI(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("DressageScoringApi");
+        }
+
+        public async Task<List<?????? >> GetHorses()
+        {
+            var horses = await _httpClient.GetFromJsonAsync<List<HorseModel>>("");
+            return horses;
+        }
+    }
+}
+    }
+}
