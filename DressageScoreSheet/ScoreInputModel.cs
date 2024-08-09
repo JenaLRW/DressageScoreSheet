a01961f541a0319c15a06c3df11b25d5aa49e425
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-08-09 12:15:09 -0400

    calling the API from the console app
    Console App needed ScoreInputModel file

---
+++ DressageScoreSheet/ScoreInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DressageScoreSheet
+{
+    internal class ScoreInputModel
+    {
+        
+        public double TotalScore { get; set; }
+        public double[] Coef { get; internal set; }
+        public int Errors { get; internal set; }
+        public double[] TotalPoints { get; internal set; }
+        
+    }
+}
