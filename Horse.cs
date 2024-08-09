23d8ee7a1ba8f0c93174f2e73c39090a23ce16ff
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-08-09 12:59:27 -0400

    Update Horse.cs

--- Horse.cs
+++ Horse.cs
@@ -1,9 +1,12 @@
-﻿namespace scoringapi.Model
+namespace scoringapi.Model
 {
     public class Horse
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Test { get; set; }
+        public double Score { get; set; }
 
 
     }
